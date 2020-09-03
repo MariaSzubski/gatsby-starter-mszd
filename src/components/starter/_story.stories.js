8fb3219072b7b6e8d5857af083e8bb45cb702a1f
Maria Szubski <hello@mariaszubski.com>
2020-09-03 19:47:13 -0400

    Install Storybook

---
+++ src/components/starter/_story.stories.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import { select } from "@storybook/addon-knobs"
+import Component from "./"
+
+// Category|Group/Sub-Group
+export default {
+  title: "Title",
+}
+
+export const labelA = () => {
+  return <Component />
+}
---
+++ src/components/starter/component.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Component = props => (
+  <div>This is a sample comp</div>
+)
+
+Component.propTypes = {
+}
+
+Component.defaultProps = {
+
+}
+
+export default Component

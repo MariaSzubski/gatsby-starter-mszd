2ccfe648387dff4c76af11a4e2bb6ded01bc639e
Maria Szubski <hello@mariaszubski.com>
2020-09-03 23:23:59 -0400

    Set up default layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,32 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "../header"
+import GlobalStyles from "../global/styles"
 import SEO from "./seo"
 
-const Layout = ({ children, ...props }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <SEO title={props.title} />
-      <div>
-        <main>{children}</main>
-        <footer>© {new Date().getFullYear()} Maria Szubski Digital, LLC</footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children, ...props }) => (
+  <>
+    <SEO title={props.title} />
+    <GlobalStyles />
+    <div>
+      <main>{children}</main>
+      <footer>© {new Date().getFullYear()} Maria Szubski Digital, LLC</footer>
+    </div>
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
